d482fc2036aeb742bdcbd9697d10f436a180af2d
hasanhameed <hasanhameed858@gmail.com>
2024-07-22 21:16:32 +0530

    inc&dec by2 implemmented

--- reducx-demo.js
+++ reducx-demo.js
@@ -1,27 +1,22 @@
 const redux = require('redux');
 
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return
-        {
-            counter: state.counter + 1
-        }
+const counterReducer = (state = {counter:0} , action) =>{
+    if(action.type=== 'increment'){
+          return {
+        counter : state.counter+2
+    }};
 
-    };
-    if (action.type === 'decrement') {
-        return
-        {
-            counter: state.counter - 1
-        }
-    }
+    if(action.type=== 'decrement'){
+        return {
+      counter : state.counter-2
+  }};
 
-    return state;
 };
-
+ 
 const store = redux.createStore(counterReducer);
 
-const counterSubscriber = () => {
+const counterSubscriber =()=>{
     const latestState = store.getState();
     console.log(latestState);
 };
@@ -29,5 +24,5 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'decrement' });
+store.dispatch({type : 'increment'});
+store.dispatch({type : 'decrement'});
